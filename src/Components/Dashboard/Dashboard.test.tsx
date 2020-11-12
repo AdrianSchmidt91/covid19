fa5ccb6ca64c3753056ff0f78bc2a6e917b3a8cd
Adrian Schmidt <adrian.schmidt1991@gmail.com>
2020-11-12 19:47:42 +0100

    deleted tests

--- src/Components/Dashboard/Dashboard.test.tsx
+++ src/Components/Dashboard/Dashboard.test.tsx
@@ -5,27 +5,5 @@ import Dashboard from './Dashboard';
 afterEach(cleanup);
 
 describe('Dashboard', () => {
-  it('should render correctly', () => {
-    render(<Dashboard />);
-  });
 
-  it('should render the heading', () => {
-    render(<Dashboard />);
-    const heading = screen.getByRole('heading', {
-      name: /covid 19 statistic \- germany/i,
-    });
-    expect(heading).toHaveTextContent('Covid 19 Statistic - Germany');
-  });
-
-  it('should show select a country', () => {
-    render(<Dashboard />);
-    const text = screen.getByText(/select a country:/i);
-    expect(text).toHaveTextContent('Select a Country');
-  });
-
-  it('should show a select fiel for the countries', () => {
-    render(<Dashboard />);
-    const select = screen.getByRole('combobox');
-    expect(select).toBeInTheDocument();
-  });
 });
